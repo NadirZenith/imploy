c669b052fcb408e24846a87683e54293f376040a
NadirZenith <2cb.md2@gmail.com>
2017-02-04 02:23:05 +0100

    setup

--- src/AppBundle/Controller/DeployController.php
+++ src/AppBundle/Controller/DeployController.php
@@ -4,20 +4,21 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
-class DeployControllerController extends Controller
+class DeployController extends Controller
 {
     /**
      * @Route("/")
      */
-    public function mainAction( )
+    public function mainAction(Request $request )
     {   
 	$dir = $this->getParameter('kernel.root_dir') . '/../web/';
 	$filename = 'test.html';
 	$file = $dir . $filename;
 
 	ob_start();	
-	d($file);
+	d($request);
 	$content = ob_get_clean();
 
 	file_put_contents($file, $content, FILE_APPEND | LOCK_EX);
