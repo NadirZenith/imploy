8243a51e81aeee91670b2e145267285826089ced
Nadir Zenith <nz@fame.pt>
2017-02-04 01:21:02 +0000

    setup

--- src/AppBundle/Controller/DeployControllerController.php
+++ src/AppBundle/Controller/DeployControllerController.php
@@ -10,8 +10,18 @@ class DeployControllerController extends Controller
     /**
      * @Route("/")
      */
-    public function mainAction()
-    {
+    public function mainAction( )
+    {   
+	$dir = $this->getParameter('kernel.root_dir') . '/../web/';
+	$filename = 'test.html';
+	$file = $dir . $filename;
+
+	ob_start();	
+	d($file);
+	$content = ob_get_clean();
+
+	file_put_contents($file, $content, FILE_APPEND | LOCK_EX);
+	
         return $this->render('AppBundle:DeployController:main.html.twig', array(
             // ...
         ));
